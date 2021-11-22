fc3ddee5e760f1e453dc3e05f69b197f5ad9a447
DavTech <dav-tech@outlook.com>
2021-11-22 15:09:11 +0100

    Fixes url issues

--- config/settings.js
+++ config/settings.js
@@ -2,5 +2,5 @@ const PORT = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
-  ? `http://localhost:${PORT}/`
-  : `http://localhost:${PORT}/`;
+  ? `http://localhost:${PORT}`
+  : `http://localhost:${PORT}`;
