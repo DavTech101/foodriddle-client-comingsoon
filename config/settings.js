828342d1fb0c46d21f6bcaa1dcf59ae27b6d85d9
DavTech <dav-tech@outlook.com>
2021-11-22 16:32:58 +0100

    Fixes url issues part 5

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,6 @@
 const PORT = 3000;
 const dev = process.env.NEXT_PUBLIC_VERCEL_ENV == 'development';
 
-export const server = dev ? `http://localhost:${PORT}` : ``;
+export const server = dev
+  ? `http://localhost:${PORT}`
+  : `http://localhost:3000`;
