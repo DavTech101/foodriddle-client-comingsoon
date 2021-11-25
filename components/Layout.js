186447e4b761286597dddb9d04ff8fefea71fa29
DavTech <dav-tech@outlook.com>
2021-11-25 13:24:11 +0100

    Puts scroller to invisible when not needed

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,11 @@ const PageContainer = tw.div`flex flex-row relative justify-center`;
 const BodyContainer = styled.div`
   ${tw`absolute left-0 py-6 px-0.5 flex flex-col justify-between`}
   ${tw`min-h-screen h-screen frsignup:w-100 items-center `}
-  ${tw`bg-white bg-opacity-95 rounded-3xl overflow-auto`}
+  ${tw`bg-white bg-opacity-95 rounded-3xl overflow-hidden`}
+
+  &:hover, &:active, &:focus {
+    ${tw`overflow-auto`}
+  }
 `;
 
 const Main = tw.main`py-10 px-0 flex flex-col justify-center items-center`;
