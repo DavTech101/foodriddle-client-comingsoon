7f70c70c2047194760a7bd36429f7ce591ef12f7
DavTech <dav-tech@outlook.com>
2021-11-25 14:00:56 +0100

    Puts scroller to invisible when not needed part 4

--- components/Layout.js
+++ components/Layout.js
@@ -4,13 +4,16 @@ import Footer from './Footer';
 import Header from '../components/Header';
 
 //######### Component Styles #################
-const PageContainer = tw.div`flex flex-row relative justify-center min-h-screen w-full overflow-hidden`;
+const PageContainer = tw.div`flex flex-row relative justify-center min-h-screen w-full`;
 
 const BodyContainer = styled.div`
   ${tw`absolute left-0 py-6 px-0.5 flex flex-col justify-between`}
   ${tw`min-h-screen h-screen frsignup:w-100 items-center `}
-  ${tw`bg-white bg-opacity-95 rounded-3xl`}
-  ${tw`overflow-auto`}
+  ${tw`bg-white bg-opacity-95 rounded-3xl overflow-hidden`}
+
+  &:hover, &:active, &:focus {
+    ${tw`overflow-auto`}
+  }
 `;
 
 const Main = tw.main`py-10 px-0 flex flex-col justify-center items-center`;
