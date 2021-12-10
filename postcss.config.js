6c9681fc9b813fbc4afdf5dcb6f48dac10d5057f
Thierry Davelaar <Cosito@MacBook-Pro.local>
2021-12-10 23:42:47 +0100

    configs to tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer'],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
 };
