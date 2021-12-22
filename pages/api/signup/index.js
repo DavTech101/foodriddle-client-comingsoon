8cd24167994b4d1f949d97a289c31642240b4477
Thierry Davelaar <Cosito@MacBook-Pro.local>
2021-12-22 23:15:48 +0100

    console log added

--- pages/api/signup/index.js
+++ pages/api/signup/index.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  let config = {
-    method: 'post',
-    url: `${process.env.SERVER_WEBSITE}/api/signup`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    let config = {
+        method: 'post',
+        url: `${process.env.SERVER_WEBSITE}/api/signup`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
 
-    data: JSON.stringify(req.body),
-  };
+        data: JSON.stringify(req.body),
+    };
 
-  try {
-    const response = await axios(config);
-
-    res.status(response.status).json({ message: 'successfully signed up' });
-  } catch (error) {
-    res.status(error.response.status).json(error.response.data);
-  }
+    try {
+        const response = await axios(config);
+        console.log('signed up');
+        res.status(response.status).json({ message: 'successfully signed up' });
+    } catch (error) {
+        res.status(error.response.status).json(error.response.data);
+    }
 }
